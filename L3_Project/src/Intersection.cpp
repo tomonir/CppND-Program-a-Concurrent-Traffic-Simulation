d123ffc8874081b1ec9f41d51c5cfd5af4caaac3
Unknown <tomonir@gmail.com>
2020-05-28 10:03:04 +0200

    fix: L3 exercise

--- L3_Project/src/Intersection.cpp
+++ L3_Project/src/Intersection.cpp
@@ -3,6 +3,7 @@
 #include <chrono>
 #include <future>
 #include <random>
+#include <mutex>
 
 #include "Street.h"
 #include "Intersection.h"
@@ -12,20 +13,26 @@
 
 // L3.1 : Safeguard all accesses to the private members _vehicles and _promises with an appropriate locking mechanism, 
 // that will not cause a deadlock situation where access to the resources is accidentally blocked.
-
+std::mutex mutex_vehicle_promises;
 int WaitingVehicles::getSize()
 {
+    std::lock_guard<std::mutex> lock(mutex_vehicle_promises);
+
     return _vehicles.size();
 }
 
 void WaitingVehicles::pushBack(std::shared_ptr<Vehicle> vehicle, std::promise<void> &&promise)
 {
+    std::lock_guard<std::mutex> lock(mutex_vehicle_promises);
+
     _vehicles.push_back(vehicle);
     _promises.push_back(std::move(promise));
 }
 
 void WaitingVehicles::permitEntryToFirstInQueue()
 {
+    std::lock_guard<std::mutex> lock(mutex_vehicle_promises);
+
     // get entries from the front of both queues
     auto firstPromise = _promises.begin();
     auto firstVehicle = _vehicles.begin();
@@ -70,9 +77,10 @@ std::vector<std::shared_ptr<Street>> Intersection::queryStreets(std::shared_ptr<
 void Intersection::addVehicleToQueue(std::shared_ptr<Vehicle> vehicle)
 {
     // L3.3 : Ensure that the text output locks the console as a shared resource. Use the mutex _mtxCout you have added to the base class TrafficObject in the previous task. Make sure that in between the two calls to std-cout at the beginning and at the end of addVehicleToQueue the lock is not held. 
-
+    std::unique_lock<std::mutex> lock(_mtxCout);
+    
     std::cout << "Intersection #" << _id << "::addVehicleToQueue: thread id = " << std::this_thread::get_id() << std::endl;
-
+    lock.unlock();
     // add new vehicle to the end of the waiting line
     std::promise<void> prmsVehicleAllowedToEnter;
     std::future<void> ftrVehicleAllowedToEnter = prmsVehicleAllowedToEnter.get_future();
@@ -80,6 +88,7 @@ void Intersection::addVehicleToQueue(std::shared_ptr<Vehicle> vehicle)
 
     // wait until the vehicle is allowed to enter
     ftrVehicleAllowedToEnter.wait();
+    lock.lock();
     std::cout << "Intersection #" << _id << ": Vehicle #" << vehicle->getID() << " is granted entry." << std::endl;
 }
 
